5db60c66b7c6170d2204c322858a1a2ac3fd052e
root <root@alpha.nucyfer.local>
2025-11-08 14:50:06 -0500

    Changed the generate_vmbr.py script to not add vmbrs to the host by default

--- Advanced_Scripts_Modules/generate_vmbrs.py
+++ Advanced_Scripts_Modules/generate_vmbrs.py
@@ -2,7 +2,8 @@
 import os
 
 def run():
-	f = open("new_interfaces_file.txt", "a+")
+	s = "empty"
+	f = open("new_interfaces_file.txt", "a")
 	startVMBR = int(input("What did you want the vmbr's to start at? "))
 	if startVMBR <= 0:
 		print ("Not a valid input")
@@ -11,10 +12,21 @@ def run():
 	if startVMBR > numVMBR:
 		print ("Number is not large enough")
 		return 
-	startIP = input("What is the ip address?")
-	for x in range(startVMBR, numVMBR + 1):
-		s = "auto vmbr" + str(x) + "\niface vmbr" + str(x) + " inet static\n" + "    address  " + startIP + "\n    netmask  255.255.255.0" + "\n    bridge_ports none" + "\n    bridge_stp off" + "\n    bridge_fd 0\n\n"
-		f.write(s)
-	f.close()
+	connectToHost = input("Do you want the host to be accessible from this vmbr? Y/N: ")
+	if connectToHost == "Y" or connectToHost == "y" or connectToHost == "Yes" or connectToHost == "yes":
+		startIP = input("What is the ip address?")
+		for x in range(startVMBR, numVMBR + 1):
+			s = "auto vmbr" + str(x) + "\niface vmbr" + str(x) + " inet static\n" + "    address  " + startIP + "\n    netmask  255.255.255.0" + "\n    bridge_ports none" + "\n    bridge_stp off" + "\n    bridge_fd 0\n\n"
+			f.write(s)
+	elif connectToHost == "N" or connectToHost == "n" or connectToHost == "No" or connectToHost == "no":
+		print("The host will not be accessible from this vmbr.")
+		for x in range(startVMBR, numVMBR + 1):
+			s = "auto vmbr" + str(x) + "\niface vmbr" + str(x) + " inet static\n" + "    bridge_ports none" + "\n    bridge_stp off" + "\n    bridge_fd 0\n\n"
+			f.write(s)
+	else:
+		print(f"Input [{connectToHost}] is invalid. Please try again")
 
+	f.close()
+	print(f"\n***** Writing -> \n\n{s}")
+	print(f'***** Your new vmbr(s) will be in the file called "new_interfaces_file.txt".')
 	return "Success"
